c5821fb2f24a8cd79103844a2c543362500e85ee
黄少威 <huangshaowei@xmfunny.com>
2022-12-12 17:41:16 +0800

    no message

--- study-golang/json/json_test.go
+++ study-golang/json/json_test.go
@@ -4,23 +4,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"testing"
 )
 
 type Movie struct {
-	title string `json:"title"`
+	title string `json:"title"` // 自动忽略小写字母开头的字断
 	Year  int
-	F1    bool   `json:"F1,omitempty"` // 加上 omitempty 如果值为 false 将被忽略，如果是其他类型，为 nil 也将被忽略
+	F1    bool   `json:"F1,omitempty"` // 加上 omitempty 如果类型是布尔值，值为 false 将被忽略；如果是其他类型，为 nil 将被忽略
 	F2    bool   `json:"F2"`
-	F3    string `json:"-"` // 如果内容只包含 - ，则表示忽略该字段，需要特别注意，如果内容为 -, 则表示 json 字段名为 -
+	F3    string `json:"-"`  // 表示忽略该字段
+	F4    string `json:"-,"` // 表示 json 字段名为 -
 }
 
-func test() {
+func Test_json(t *testing.T) {
 	movie := Movie{
 		title: "Casablanca",
 		Year:  1942,
 		F1:    false,
 		F2:    false,
 		F3:    "Casablanca",
+		F4:    "Casablanca",
 	}
 
 	// 序列化
