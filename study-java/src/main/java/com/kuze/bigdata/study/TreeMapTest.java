69910f4880071263ef2510a0311c010b80c4fdfe
黄少威 <huangshaowei@xmfunny.com>
2023-02-10 18:41:47 +0800

    no message

---
+++ study-java/src/main/java/com/kuze/bigdata/study/TreeMapTest.java
@@ -0,0 +1,15 @@
+package com.kuze.bigdata.study;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class TreeMapTest {
+
+    public static void main(String[] args) {
+
+        SortedMap<Long, String> tm = new TreeMap<>();
+
+
+    }
+
+}
