da4822fead80f6addea85360b1ef0300f4816ba3
黄少威 <huangshaowei@xmfunny.com>
2023-06-27 14:45:09 +0800

    no message

--- study-golang/go.mod
+++ study-golang/go.mod
@@ -78,6 +78,7 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- study-golang/rate/rate_test.go
+++ study-golang/rate/rate_test.go
@@ -1,7 +1,25 @@
 package rate
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"golang.org/x/time/rate"
+	"testing"
+	"time"
+)
 
 func Test_rate(t *testing.T) {
+
+	//初始化 limiter 每秒10个令牌，令牌桶容量为20
+	limiter := rate.NewLimiter(2, 3)
+
+	for {
+		err := limiter.WaitN(context.Background(), 1)
+		if err != nil {
+			fmt.Println("error", err)
+		} else {
+			fmt.Println("success get token", time.Now())
+		}
+	}
 
 }
