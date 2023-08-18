e6f81d81bda8970fa2da6f4c1d2c8e8b0134abf4
黄少威 <huangshaowei@xmfunny.com>
2023-08-18 16:59:05 +0800

    no message

---
+++ study-golang/defer/defer_test.go
@@ -0,0 +1,40 @@
+package _defer
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func test1() int {
+	var tmp = 0
+	defer func() {
+		tmp = 1
+	}()
+	return tmp
+}
+
+func test2() (result int) {
+	defer func() {
+		result = 1
+	}()
+	return result
+}
+
+func test3() (result error) {
+	return result
+}
+
+func test4() (result error) {
+	defer func() {
+		result = errors.New("test")
+	}()
+	return result
+}
+
+func Test_defer(t *testing.T) {
+	fmt.Println(test1())
+	fmt.Println(test2())
+	fmt.Println(test3())
+	fmt.Println(test4())
+}
