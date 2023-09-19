a122e8c06d732259141724e97f0be3423d28df8a
黄少威 <huangshaowei@xmfunny.com>
2023-09-19 10:38:47 +0800

    no message

--- study-golang/hash/hmac_test.go
+++ study-golang/hash/hmac_test.go
@@ -11,15 +11,15 @@ import (
 
 func Test_hmac(t *testing.T) {
 
-	h := hmac.New(sha256.New, []byte("CsYyCc=="))
+	h := hmac.New(sha256.New, []byte("demo"))
 
-	h.Write([]byte("GET"))
-	h.Write([]byte("/v1/metadata"))
-	h.Write([]byte("rQJEk4mzg"))
+	h.Write([]byte("POST"))
+	h.Write([]byte("/v1/collect"))
+	h.Write([]byte("demo"))
 	h.Write([]byte("123"))
 	h.Write([]byte("123"))
 	// 写入 nil 和 不调用的结果一样
-	h.Write(nil)
+	h.Write([]byte("{\"batchId\":\"123\",\"messages\":[{\"type\":\"event\",\"data\":{\"a\":3,\"b\":\"3\"}}]}"))
 
 	fmt.Println(base64.StdEncoding.EncodeToString(h.Sum(nil)))
 }
