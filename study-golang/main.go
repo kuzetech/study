45c9d378c86ac1eeaf123eac2a3edf601d8bcae2
黄少威 <huangshaowei@xmfunny.com>
2024-01-18 11:39:44 +0800

    no message

--- study-golang/main.go
+++ study-golang/main.go
@@ -1,15 +1,29 @@
 package main
 
-import (
-	"log"
-)
+import "fmt"
 
 func main() {
+	// 原始数组
+	originalArray := [8]int{1, 2, 3, 4, 5, 6, 7, 8}
 
-	var m = make(map[interface{}]interface{})
+	// 将数组四等分
+	length := len(originalArray)
+	quarterSize := length / 4
 
-	m[1] = 1
-	m["a"] = "a"
+	// 使用 for 循环切片
+	for i := 0; i < 4; i++ {
+		start := i * quarterSize
+		end := (i + 1) * quarterSize
 
-	log.Println("test", m)
+		// 防止索引越界
+		if end > length {
+			end = length
+		}
+
+		// 切片
+		slice := originalArray[start:end]
+
+		// 打印结果
+		fmt.Printf("Slice %d: %v\n", i+1, slice)
+	}
 }
